3919d3c8637f645b268306877c82fbc2d96a4376
valentinfrlch <valentin.froehlich@gmail.com>
2022-06-16 12:22:03 +0200

    json doesn't convert correctly yet

--- get.py
+++ get.py
@@ -13,7 +13,19 @@ def get_wait_times():
 
 
 def parse_wait_times(data):
+    rides = []
     wait_times = []
-    for item in data:
-        wait_times.append(item['wait_time'])
-    return wait_times
+    for item in data["lands"]:
+        #append rides to rides list
+        rides.append(item["rides"])
+    for ride in rides:
+        #append wait times to wait_times list
+        # read ride as json object
+        print(ride[0])
+        ride = json.loads(ride[0])
+        print(ride["wait_time"])
+        wait_times.append([ride["id"], ride["wait_time"]])
+    return rides
+
+
+print(parse_wait_times(get_wait_times()))
